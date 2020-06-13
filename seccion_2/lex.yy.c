2086fd3aeaffa30bdbf2fd8925c9af0b585fd158
ErickHCerecedo <erickcerecedo@gmail.com>
2020-06-13 15:22:52 -0500

    seccion_2 fb2-4.l

--- seccion_2/lex.yy.c
+++ seccion_2/lex.yy.c
@@ -262,11 +262,15 @@ static void yy_flex_free YY_PROTO(( void * ));
 #define YY_AT_BOL() (yy_current_buffer->yy_at_bol)
 
 
+#define YY_USES_REJECT
+
 #define yywrap() 1
 #define YY_SKIP_YYWRAP
 typedef unsigned char YY_CHAR;
 FILE *yyin = (FILE *) 0, *yyout = (FILE *) 0;
 typedef int yy_state_type;
+extern int yylineno;
+int yylineno = 1;
 extern char *yytext;
 #define yytext_ptr yytext
 
@@ -285,11 +289,25 @@ static void yy_fatal_error YY_PROTO(( yyconst char msg[] ));
 	*yy_cp = '\0'; \
 	yy_c_buf_p = yy_cp;
 
-#define YY_NUM_RULES 4
-#define YY_END_OF_BUFFER 5
-static yyconst short int yy_accept[9] =
+#define YY_NUM_RULES 22
+#define YY_END_OF_BUFFER 23
+static yyconst short int yy_acclist[70] =
     {   0,
-        0,    0,    5,    3,    2,    1,    1,    0
+       23,   21,   22,    1,   20,   21,   22,   20,   21,   22,
+       20,   21,   22,   20,   21,   22,   20,   21,   22,   20,
+       21,   22,   20,   21,   22,   20,    2,   20,   20,    5,
+       20,    6,   20,    7,   20,   20,   20,   10,   20,   11,
+       20,   12,   20,   13,   20,   14,   20,   15,   20,   20,
+       19,   20,   20,    3,   20,    4,   20,    8,   20,    9,
+       20,   20,   17,   20,   20,   16,   20,   18,   20
+    } ;
+
+static yyconst short int yy_accept[41] =
+    {   0,
+        1,    1,    1,    2,    4,    8,   11,   14,   17,   20,
+       23,   26,   26,   27,   29,   30,   32,   34,   36,   37,
+       38,   40,   42,   44,   46,   48,   50,   51,   53,   54,
+       56,   58,   60,   62,   63,   65,   66,   68,   70,   70
     } ;
 
 static yyconst int yy_ec[256] =
@@ -297,17 +315,17 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1,    1,    1,    1,    1,    2,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    1,    3,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
-        1,    1,    1,    1,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        1,    1,    1,    1,    1,    1,    3,    3,    3,    3,
+        1,    1,    1,    1,    4,    5,    6,    7,    8,    9,
+        6,   10,   11,    6,    6,    6,    6,   12,   13,    6,
+        6,   14,   15,   16,   17,    6,    6,    6,    6,    6,
+        1,    1,    1,    1,    1,    1,    4,    5,    6,    7,
 
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    3,    3,    3,    3,    3,    3,    3,    3,
-        3,    3,    1,    1,    1,    1,    1,    1,    1,    1,
+        8,    9,    6,   10,   11,    6,    6,    6,    6,   12,
+       13,    6,    6,   14,   15,   16,   17,    6,    6,    6,
+        6,    6,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
         1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
@@ -324,53 +342,101 @@ static yyconst int yy_ec[256] =
         1,    1,    1,    1,    1
     } ;
 
-static yyconst int yy_meta[4] =
+static yyconst int yy_meta[18] =
     {   0,
-        1,    1,    2
+        1,    1,    2,    2,    2,    2,    2,    2,    2,    2,
+        2,    2,    2,    2,    2,    2,    2
     } ;
 
-static yyconst short int yy_base[10] =
+static yyconst short int yy_base[41] =
     {   0,
-        0,    0,    5,    6,    6,    0,    0,    6,    2
+        0,    0,   68,   93,   15,   29,   63,   20,   44,   58,
+       70,    4,   62,   18,   31,   61,   60,   59,   19,   35,
+       55,   54,   52,   51,   50,   49,   81,   48,   93,   47,
+       45,   42,   41,   25,   40,   21,   39,   37,   93,   24
     } ;
 
-static yyconst short int yy_def[10] =
+static yyconst short int yy_def[41] =
     {   0,
-        8,    1,    8,    8,    8,    9,    9,    0,    8
+       39,    1,   39,   39,   40,   40,   40,   40,   40,   40,
+       40,   39,   40,   40,   40,   40,   40,   40,   40,   40,
+       40,   40,   40,   40,   40,   40,   40,   40,   39,   40,
+       40,   40,   40,   40,   40,   40,   40,   40,    0,   39
     } ;
 
-static yyconst short int yy_nxt[10] =
+static yyconst short int yy_nxt[111] =
     {   0,
-        4,    5,    6,    7,    8,    3,    8,    8,    8
+        4,    4,    4,    5,    6,    7,    7,    7,    8,    7,
+        9,    7,   10,    7,    7,   11,    7,   12,   29,   29,
+       12,   12,   12,   12,   30,   13,   14,   12,   15,   16,
+       17,   12,   20,   12,   32,   38,   18,   12,   31,   12,
+       37,   12,   12,   12,   12,   19,   12,   12,   33,   12,
+       12,   12,   12,   12,   12,   21,   12,   12,   22,   23,
+       12,   12,   12,   12,   12,   12,   24,   39,   39,   25,
+       39,   26,   12,   39,   39,   39,   39,   39,   39,   27,
+       39,   39,   28,   12,   34,   39,   39,   39,   35,   39,
+       39,   36,    3,   39,   39,   39,   39,   39,   39,   39,
+
+       39,   39,   39,   39,   39,   39,   39,   39,   39,   39
     } ;
 
-static yyconst short int yy_chk[10] =
+static yyconst short int yy_chk[111] =
     {   0,
-        1,    1,    1,    9,    3,    8,    8,    8,    8
+        1,    1,    1,    1,    1,    1,    1,    1,    1,    1,
+        1,    1,    1,    1,    1,    1,    1,    5,   12,   12,
+       14,   19,    8,   36,   14,   40,    5,   34,    5,    5,
+        5,    6,    8,   15,   19,   36,    6,   20,   15,   38,
+       34,   37,   35,   33,   32,    6,    9,   31,   20,   30,
+       28,   26,   25,   24,   23,    9,   22,   21,    9,    9,
+       10,   18,   17,   16,   13,    7,   10,    3,    0,   10,
+        0,   10,   11,    0,    0,    0,    0,    0,    0,   11,
+        0,    0,   11,   27,   27,    0,    0,    0,   27,    0,
+        0,   27,   39,   39,   39,   39,   39,   39,   39,   39,
+
+       39,   39,   39,   39,   39,   39,   39,   39,   39,   39
     } ;
 
-static yy_state_type yy_last_accepting_state;
-static char *yy_last_accepting_cpos;
-
-/* The intent behind this definition is that it'll catch
- * any uses of REJECT which flex missed.
- */
-#define REJECT reject_used_but_not_detected
+static yy_state_type yy_state_buf[YY_BUF_SIZE + 2], *yy_state_ptr;
+static char *yy_full_match;
+static int yy_lp;
+#define REJECT \
+{ \
+*yy_cp = yy_hold_char; /* undo effects of setting up yytext */ \
+yy_cp = yy_full_match; /* restore poss. backed-over text */ \
+++yy_lp; \
+goto find_rule; \
+}
 #define yymore() yymore_used_but_not_detected
 #define YY_MORE_ADJ 0
 #define YY_RESTORE_YY_MORE_OFFSET
 char *yytext;
-#line 1 "fb2-2.l"
+#line 1 "fb2-4.l"
 #define INITIAL 0
-/* fb2-2 read several files */
-#line 4 "fb2-2.l"
-int chars = 0;
-int words = 0;
-int lines = 0;
-int totchars = 0;
-int totwords = 0;
-int totlines = 0;
-#line 374 "lex.yy.c"
+/* fb2-4 text concordance */
+/* the symbol table */
+#line 6 "fb2-4.l"
+struct symbol { /* a word */
+    char *name;
+    struct ref *reflist;
+};
+
+struct ref {
+    struct ref *next;
+    char *filename;
+    int flags;
+    int lineno;
+};
+
+/* simple symtab of fixed size */
+#define NHASH 9997
+struct symbol symtab[NHASH];
+
+struct symbol *lookup(char*);
+void addref(int, char*, char*,int);
+
+char *curfilename; /* name of current input file */
+
+#line 440 "lex.yy.c"
 
 /* Macros after this point can all be overridden by user definitions in
  * section 1.
@@ -521,9 +587,11 @@ YY_DECL
 	register char *yy_cp, *yy_bp;
 	register int yy_act;
 
-#line 12 "fb2-2.l"
+#line 30 "fb2-4.l"
 
-#line 527 "lex.yy.c"
+ /* rules for concordance generator */
+ /* skip common words */
+#line 595 "lex.yy.c"
 
 	if ( yy_init )
 		{
@@ -562,73 +630,117 @@ YY_DECL
 		yy_bp = yy_cp;
 
 		yy_current_state = yy_start;
+		yy_state_ptr = yy_state_buf;
+		*yy_state_ptr++ = yy_current_state;
 yy_match:
 		do
 			{
 			register YY_CHAR yy_c = yy_ec[YY_SC_TO_UI(*yy_cp)];
-			if ( yy_accept[yy_current_state] )
-				{
-				yy_last_accepting_state = yy_current_state;
-				yy_last_accepting_cpos = yy_cp;
-				}
 			while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 				{
 				yy_current_state = (int) yy_def[yy_current_state];
-				if ( yy_current_state >= 9 )
+				if ( yy_current_state >= 40 )
 					yy_c = yy_meta[(unsigned int) yy_c];
 				}
 			yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
+			*yy_state_ptr++ = yy_current_state;
 			++yy_cp;
 			}
-		while ( yy_base[yy_current_state] != 6 );
+		while ( yy_base[yy_current_state] != 93 );
 
 yy_find_action:
-		yy_act = yy_accept[yy_current_state];
-		if ( yy_act == 0 )
-			{ /* have to back up */
-			yy_cp = yy_last_accepting_cpos;
-			yy_current_state = yy_last_accepting_state;
-			yy_act = yy_accept[yy_current_state];
+		yy_current_state = *--yy_state_ptr;
+		yy_lp = yy_accept[yy_current_state];
+find_rule: /* we branch to this label when backing up */
+		for ( ; ; ) /* until we find what rule we matched */
+			{
+			if ( yy_lp && yy_lp < yy_accept[yy_current_state + 1] )
+				{
+				yy_act = yy_acclist[yy_lp];
+					{
+					yy_full_match = yy_cp;
+					break;
+					}
+				}
+			--yy_cp;
+			yy_current_state = *--yy_state_ptr;
+			yy_lp = yy_accept[yy_current_state];
 			}
 
 		YY_DO_BEFORE_ACTION;
 
+		if ( yy_act != YY_END_OF_BUFFER )
+			{
+			int yyl;
+			for ( yyl = 0; yyl < yyleng; ++yyl )
+				if ( yytext[yyl] == '\n' )
+					++yylineno;
+			}
 
 do_action:	/* This label is used only to access EOF actions. */
 
 
 		switch ( yy_act )
 	{ /* beginning of action switch */
-			case 0: /* must back up */
-			/* undo the effects of YY_DO_BEFORE_ACTION */
-			*yy_cp = yy_hold_char;
-			yy_cp = yy_last_accepting_cpos;
-			yy_current_state = yy_last_accepting_state;
-			goto yy_find_action;
-
 case 1:
-YY_RULE_SETUP
-#line 13 "fb2-2.l"
-{ words++; chars += strlen(yytext); }
-	YY_BREAK
+#line 34 "fb2-4.l"
 case 2:
-YY_RULE_SETUP
-#line 14 "fb2-2.l"
-{ chars++; lines++; }
-	YY_BREAK
+#line 35 "fb2-4.l"
 case 3:
-YY_RULE_SETUP
-#line 15 "fb2-2.l"
-{ chars++; }
-	YY_BREAK
+#line 36 "fb2-4.l"
 case 4:
+#line 37 "fb2-4.l"
+case 5:
+#line 38 "fb2-4.l"
+case 6:
+#line 39 "fb2-4.l"
+case 7:
+#line 40 "fb2-4.l"
+case 8:
+#line 41 "fb2-4.l"
+case 9:
+#line 42 "fb2-4.l"
+case 10:
+#line 43 "fb2-4.l"
+case 11:
+#line 44 "fb2-4.l"
+case 12:
+#line 45 "fb2-4.l"
+case 13:
+#line 46 "fb2-4.l"
+case 14:
+#line 47 "fb2-4.l"
+case 15:
+#line 48 "fb2-4.l"
+case 16:
+#line 49 "fb2-4.l"
+case 17:
+#line 50 "fb2-4.l"
+case 18:
+#line 51 "fb2-4.l"
+case 19:
 YY_RULE_SETUP
-#line 16 "fb2-2.l"
-ECHO;
+#line 51 "fb2-4.l"
+/* ignore */
 	YY_BREAK
-#line 630 "lex.yy.c"
-case YY_STATE_EOF(INITIAL):
-	yyterminate();
+case 20:
+YY_RULE_SETUP
+#line 52 "fb2-4.l"
+{ addref(yylineno, curfilename, yytext, 0); }
+	YY_BREAK
+case 21:
+YY_RULE_SETUP
+#line 53 "fb2-4.l"
+/* ignore everything else */
+	YY_BREAK
+case 22:
+YY_RULE_SETUP
+#line 54 "fb2-4.l"
+YY_FATAL_ERROR( "flex scanner jammed" );
+	YY_BREAK
+#line 742 "lex.yy.c"
+			case YY_STATE_EOF(INITIAL):
+				yyterminate();
 
 	case YY_END_OF_BUFFER:
 		{
@@ -906,22 +1018,20 @@ static yy_state_type yy_get_previous_state()
 	register char *yy_cp;
 
 	yy_current_state = yy_start;
+	yy_state_ptr = yy_state_buf;
+	*yy_state_ptr++ = yy_current_state;
 
 	for ( yy_cp = yytext_ptr + YY_MORE_ADJ; yy_cp < yy_c_buf_p; ++yy_cp )
 		{
 		register YY_CHAR yy_c = (*yy_cp ? yy_ec[YY_SC_TO_UI(*yy_cp)] : 1);
-		if ( yy_accept[yy_current_state] )
-			{
-			yy_last_accepting_state = yy_current_state;
-			yy_last_accepting_cpos = yy_cp;
-			}
 		while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 			{
 			yy_current_state = (int) yy_def[yy_current_state];
-			if ( yy_current_state >= 9 )
+			if ( yy_current_state >= 40 )
 				yy_c = yy_meta[(unsigned int) yy_c];
 			}
 		yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
+		*yy_state_ptr++ = yy_current_state;
 		}
 
 	return yy_current_state;
@@ -942,22 +1052,18 @@ yy_state_type yy_current_state;
 #endif
 	{
 	register int yy_is_jam;
-	register char *yy_cp = yy_c_buf_p;
 
 	register YY_CHAR yy_c = 1;
-	if ( yy_accept[yy_current_state] )
-		{
-		yy_last_accepting_state = yy_current_state;
-		yy_last_accepting_cpos = yy_cp;
-		}
 	while ( yy_chk[yy_base[yy_current_state] + yy_c] != yy_current_state )
 		{
 		yy_current_state = (int) yy_def[yy_current_state];
-		if ( yy_current_state >= 9 )
+		if ( yy_current_state >= 40 )
 			yy_c = yy_meta[(unsigned int) yy_c];
 		}
 	yy_current_state = yy_nxt[yy_base[yy_current_state] + (unsigned int) yy_c];
-	yy_is_jam = (yy_current_state == 8);
+	yy_is_jam = (yy_current_state == 39);
+	if ( ! yy_is_jam )
+		*yy_state_ptr++ = yy_current_state;
 
 	return yy_is_jam ? 0 : yy_current_state;
 	}
@@ -1000,6 +1106,8 @@ register char *yy_bp;
 
 	*--yy_cp = (char) c;
 
+	if ( c == '\n' )
+		--yylineno;
 
 	yytext_ptr = yy_bp;
 	yy_hold_char = *yy_cp;
@@ -1076,6 +1184,8 @@ static int input()
 	*yy_c_buf_p = '\0';	/* preserve yytext */
 	yy_hold_char = *++yy_c_buf_p;
 
+	if ( c == '\n' )
+		++yylineno;
 
 	return c;
 	}
@@ -1512,35 +1622,127 @@ int main()
 	return 0;
 	}
 #endif
-#line 16 "fb2-2.l"
+#line 54 "fb2-4.l"
 
 
-main(argc, argv)
+/* concordance main routine */
+int main(argc, argv)
 int argc;
 char **argv;
 {
     int i;
     if(argc < 2) { /* just read stdin */
+        curfilename = "(stdin)";
+        yylineno = 1;
         yylex();
-        printf("%8d%8d%8d\n", lines, words, chars);
-        return 0;
-    }
+    } else
     for(i = 1; i < argc; i++) {
         FILE *f = fopen(argv[i], "r");
         if(!f) {
-            perror(argv[i]);
+            perror(argv[1]);
             return (1);
         }
+        curfilename = argv[i]; /* for addref */
         yyrestart(f);
+        yylineno = 1;
         yylex();
         fclose(f);
-        printf("%8d%8d%8d %s\n", lines, words, chars, argv[i]);
-        totchars += chars; chars = 0;
-        totwords += words; words = 0;
-        totlines += lines; lines = 0;
     }
-    if(argc > 1) /* print total if more than one file */
-        printf("%8d%8d%8d total\n", totlines, totwords, totchars);
-    
-    return 0;
+    printrefs();
+}
+
+/* hash a symbol */
+static unsigned
+symhash(char *sym)
+{
+    unsigned int hash = 0;
+    unsigned c;
+    while(c = *sym++) hash = hash*9 ^ c;
+    return hash;
+}
+
+struct symbol *
+lookup(char* sym)
+{
+    struct symbol *sp = &symtab[symhash(sym)%NHASH];
+    int scount = NHASH; /* how many have we looked at */
+    while(--scount >= 0) {
+        if(sp->name && !strcmp(sp->name, sym)) return sp;
+        if(!sp->name) { /* new entry */
+            sp->name = strdup(sym);
+            sp->reflist = 0;
+            return sp;
+        }
+        if(++sp >= symtab+NHASH) sp = symtab; /* try the next entry */
+    }
+    fputs("symbol table overflow\n", stderr);
+    abort(); /* tried them all, table is full */
+}
+
+void
+addref(int lineno, char *filename, char *word, int flags)
+{
+    struct ref *r;
+    struct symbol *sp = lookup(word);
+    /* don't do dups of same line and file */
+    if(sp->reflist &&
+    sp->reflist->lineno == lineno &&
+    sp->reflist->filename == filename) return;
+    r = malloc(sizeof(struct ref));
+    if(!r) {fputs("out of space\n", stderr); abort(); }
+    r->next = sp->reflist;
+    r->filename = filename;
+    r->lineno = lineno;
+    r->flags = flags;
+    sp->reflist = r;
+}
+
+/* print the references
+* sort the table alphabetically
+* then flip each entry's reflist to get it into forward order
+* and print it out
+*/
+/* aux function for sorting */
+static int
+symcompare(const void *xa, const void *xb)
+{
+    const struct symbol *a = xa;
+    const struct symbol *b = xb;
+    if(!a->name) {
+        if(!b->name) return 0; /* both empty */
+        return 1; /* put empties at the end */
+    }
+    if(!b->name) return -1;
+    return strcmp(a->name, b->name);
+}
+
+void
+printrefs()
+{
+    struct symbol *sp;
+    qsort(symtab, NHASH, sizeof(struct symbol), symcompare); /* sort the symbol table */
+    for(sp = symtab; sp->name && sp < symtab+NHASH; sp++) {
+        char *prevfn = NULL; /* last printed filename, to skip dups */
+        /* reverse the list of references */
+        struct ref *rp = sp->reflist;
+        struct ref *rpp = 0; /* previous ref */
+        struct ref *rpn; /* next ref */
+        do {
+            rpn = rp->next;
+            rp->next = rpp;
+            rpp = rp;
+            rp = rpn;
+        } while(rp);
+        /* now print the word and its references */
+        printf("%10s", sp->name);
+        for(rp = rpp; rp; rp = rp->next) {
+            if(rp->filename == prevfn) {
+                printf(" %d", rp->lineno);
+            } else {
+                printf(" %s:%d", rp->filename, rp->lineno);
+                prevfn = rp->filename;
+            }
+        }
+        printf("\n");
+    }
 }
