1c484b1ebd7e14955458e4c49765110524f40ac9
ErickHCerecedo <erickcerecedo@gmail.com>
2020-06-13 21:42:04 -0500

    seccion_3 fb-1

--- seccion_3/fb3-1.h
+++ seccion_3/fb3-1.h
@@ -4,20 +4,25 @@
 /* interface to the lexer */
 extern int yylineno; /* from lexer */
 void yyerror(char *s, ...);
+
 /* nodes in the abstract syntax tree */
 struct ast {
-int nodetype;
-struct ast *l;
-struct ast *r;
+    int nodetype;
+    struct ast *l;
+    struct ast *r;
 };
+
 struct numval {
-int nodetype; /* type K for constant */
-double number;
+    int nodetype; /* type K for constant */
+    double number;
 };
+
 /* build an AST */
 struct ast *newast(int nodetype, struct ast *l, struct ast *r);
 struct ast *newnum(double d);
+
 /* evaluate an AST */
 double eval(struct ast *);
+
 /* delete and free an AST */
 void treefree(struct ast *);
